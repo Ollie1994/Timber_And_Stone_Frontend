983d3e5a24951507400ed699fcd60df5fb4b456c
unknown <you@example.com>
2025-04-23 20:13:51 +0200

    Added h2 text and p text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,18 @@
 const HomePage = () => {
-    return (
-      <></>
-    );
-  };
-  export default HomePage;
-  
+  return (
+    <div className="homePage">
+      <h2>Your Dream Getaway starts here</h2>
+      <br />
+      <p>
+        Explore our cabins, cottages + off-grid retreats to start your next{" "}
+        <br />
+        adventure or find your favorite hideaway. <br />
+        See our handpicked recommendations below or use the searchbar
+        <br />
+        above to find just what you're looking for!
+      </p>{" "}
+      <br />
+    </div>
+  );
+};
+export default HomePage;
