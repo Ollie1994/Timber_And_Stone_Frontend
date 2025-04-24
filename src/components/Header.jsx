dcb99df5bd57d8b989439f0638c4a83e9d3ec39f
unknown <you@example.com>
2025-04-24 15:33:01 +0200

    added styling and images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import "../styles/header.css";
+// Imported logo as it will only take up part of header
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
       <div className="header">
-        <h1>"Logo" TIMBER + STONE</h1>
+      <div className="overlay"></div>
+      <img className="logo" src={logo} alt="Timber + Stone Logo" />
+        <h1>TIMBER + STONE</h1>
         <h2>Log in</h2>
       </div>
     </header>
