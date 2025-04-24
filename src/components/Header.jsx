a9d05b82fb51a0e4b78f86db8d01dbaa44895ced
unknown <you@example.com>
2025-04-24 13:24:06 +0200

    started css code for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import "../styles/header.css";
+
 const Header = () => {
   return (
     <header>
-      <h1>"Logo" TIMBER + STONE</h1>
-      <h2>Log in</h2>
+      <div className="header">
+        <h1>"Logo" TIMBER + STONE</h1>
+        <h2>Log in</h2>
+      </div>
     </header>
     // Remove "Logo" and add actual logo image when Header background has been added with styling
   );
