38447340723f3b98f27815853ba56b4d5876c717
unknown <you@example.com>
2025-04-24 17:06:07 +0200

    added css styling for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import "../styles/footer.css";
+import globe from "../assets/globe.png";
+
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; Timber + Stone, Inc. {' '}{' '}About {' '}{' '}Contact {' '}{' '}English (EN)</p>
+    <footer className="footer">
+      <div className="footer-left">
+      <p>&copy; Timber + Stone, Inc.</p>
+      <p className="second-paragraph">About Contact</p>
+      </div>
+      <div className="footer-right">
+      <img className="globe" src={globe} alt="Globe" />
+      <p>English (EN)</p>
+      </div>
     </footer>
     // Remove all {' '} elements once styling (Bootstrap) is added. Only added for better understanding of separate words
   );
