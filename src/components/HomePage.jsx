c62eb2be4b7d893e16733cb4b0eaa1e42f8ba272
unknown <you@example.com>
2025-04-25 20:04:27 +0200

    added all rentals inside grid col and added hover effect

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,31 +1,83 @@
 import "../styles/home.css";
+import IMG_1 from "../assets/IMG_1.png";
+import IMG_2 from "../assets/IMG_2.png";
+import IMG_3 from "../assets/IMG_3.png";
+import IMG_4 from "../assets/IMG_4.png";
+import IMG_5 from "../assets/IMG_5.png";
+import IMG_6 from "../assets/IMG_6.png";
 
 const HomePage = () => {
   return (
     <div className="home-container">
-    <div className="home-text">
-      <h2>Your Dream Getaway starts here</h2>
-      <br />
-      <article>
-        Explore our cabins, cottages + off-grid retreats to start your next
+      <div className="home-text">
+        <h2>Your Dream Getaway starts here</h2>
         <br />
-        adventure or find your favorite hideaway. <br />
-        See our handpicked recommendations below or use the searchbar
+        <article>
+          Explore our cabins, cottages + off-grid retreats to start your next
+          <br />
+          adventure or find your favorite hideaway. <br />
+          See our handpicked recommendations below or use the searchbar
+          <br />
+          above to find just what you're looking for!
+        </article>
         <br />
-        above to find just what you're looking for!
-      </article>
-      <br />
       </div>
       <div className="flex-grid">
-  <div className="col">
-    <div className="col-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. </div>
-  </div>
-  <div className="col"></div>
-  <div className="col"></div>
-  <div className="col"></div>
-  <div className="col"></div>
-  <div className="col"></div>
-</div> 
+        <div className="col">
+        <img className="IMG_1" src={IMG_1} alt="Rental 1" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+        <div className="col">
+        <img className="IMG_2" src={IMG_2} alt="Rental 2" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+        <div className="col">
+        <img className="IMG_3" src={IMG_3} alt="Rental 3" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+        <div className="col">
+        <img className="IMG_4" src={IMG_4} alt="Rental 4" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+        <div className="col">
+        <img className="IMG_5" src={IMG_5} alt="Rental 5" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+        <div className="col">
+        <img className="IMG_6" src={IMG_6} alt="Rental 6" />
+          <div style={{ flexGrow: 1 }} />
+          <div className="col-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.{" "}
+          </div> 
+        </div>
+      </div>
     </div>
   );
 };
