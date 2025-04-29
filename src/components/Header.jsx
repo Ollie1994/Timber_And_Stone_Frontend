3aff6682b08fae53c366b16e41ba242e51e89856
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-04-29 15:46:22 +0200

    logoutfunc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,36 @@ import "../styles/header.css";
 import logo from "../assets/logo.png";
 // To check if user is logged in
 import { useAuth } from "../hooks/useAuth";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { isLoggedIn } = useAuth();
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  // logga ut funktion
+  const handleLogout = async () => {
+    await logout();
+    navigate("/loginPage");
+  };
+
   return (
     <header>
       <div className="header">
         <div className="overlay"></div>
         <img className="logo" src={logo} alt="Timber + Stone Logo" />
         <h1>TIMBER + STONE</h1>
-        {isLoggedIn ? <h2>Log out</h2> : <h2>Log in</h2>}
+
+        {isLoggedIn ? (
+          <button className="logButton" onClick={handleLogout}>
+            Log out
+          </button>
+        ) : (
+          <Link className="link" to="/loginPage">
+            <button className="logButton">Login</button>
+          </Link>
+        )}
       </div>
     </header>
   );
