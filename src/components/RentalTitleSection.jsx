c01b8191464f53a672e22eda030749ba72134e2e
SatangSagwa <emilia.ivarsson@live.se>
2025-05-03 17:50:38 +0200

    getting ID from url, showing data from API. Updated CSS to keep a 6rem side-margin.

--- src/components/RentalTitleSection.jsx
+++ src/components/RentalTitleSection.jsx
@@ -3,15 +3,38 @@ import Divider from "./Divider";
 import ratingStar from "../assets/rating-star.svg";
 import shareIcon from "../assets/share-icon.svg";
 import favoriteIcon from "../assets/favorite-icon.svg";
+import { getRentalById } from "../api/rentalService";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const RentalTitleSection = () => {
+  const [rental, setRental] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchRental = async () => {
+      try {
+        const data = await getRentalById(id);
+        setRental(data);
+      } catch (err) {
+        console.log("Error " + err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRental();
+  }, []);
+
   return (
     <div>
       <div className="flex-container vertical">
-        <h2>Title</h2>
+        <h2>{rental.title}</h2>
         <div className="flex-container h-left h-center">
           <img src={locationIcon} alt="Location Icon" />
-          <h4>City, Country</h4>
+          <h4>
+            {rental.city}, {rental.country}
+          </h4>
         </div>
       </div>
 
@@ -20,14 +43,14 @@ const RentalTitleSection = () => {
       <div className="flex-container h-space-between">
         <div className="half-container">
           <img src={ratingStar} alt="Star Icon" />
-          <h5>X/5</h5>
-          <h5>X Reviews</h5>
+          <h4>{rental.averageRating}/5</h4>
+          <h4>X Reviews</h4>
         </div>
         <div className="half-container">
           <img src={shareIcon} alt="Share Icon" />
-          <h5>SHARE</h5>
+          <h4>SHARE</h4>
           <img src={favoriteIcon} alt="Favorite Icon" />
-          <h5>FAVORITE</h5>
+          <h4>FAVORITE</h4>
         </div>
       </div>
     </div>
