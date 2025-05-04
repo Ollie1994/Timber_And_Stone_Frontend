038dfb58ea7d262f425bd047f7fca5203490dccb
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-05-04 18:42:06 +0200

    some styling

--- src/components/Amenities.jsx
+++ src/components/Amenities.jsx
@@ -31,27 +31,29 @@ const Amenities = () => {
       </button>
 
       <div className={popup}>
-        {amenities.length > 0 ? (
-          amenities.map((amenity) => (
-            <div key={amenity.amenityTitle}>
-              {amenity.amenityTitle && (
-                <div className="amenity">
-                  <div className="amenityIcon">
-                    <img src={amenityIcon} alt="Amenity icon"></img>
+        <div className="amenitiesContainer">
+          {amenities.length > 0 ? (
+            amenities.map((amenity) => (
+              <div key={amenity.amenityTitle}>
+                {amenity.amenityTitle && (
+                  <div className="amenity">
+                    <div className="amenityIcon">
+                      <img src={amenityIcon} alt="Amenity icon"></img>
+                    </div>
+                    <div className="textContainer">
+                      <p className="amenityTitle">{amenity.amenityTitle}</p>
+                      <p className="amentiyDescription">
+                        {amenity.description}
+                      </p>
+                    </div>
                   </div>
-                  <div className="textContainer">
-                    <p className="amenityTitle">{amenity.amenityTitle}</p>
-                    <p className="amentiyDescription">
-                      {amenity.description}
-                    </p>
-                  </div>
-                </div>
-              )}
-            </div>
-          ))
-        ) : (
-          <div>No matching amens</div>
-        )}
+                )}
+              </div>
+            ))
+          ) : (
+            <div>No matching amens</div>
+          )}
+        </div>
 
         <button
           type="button"
