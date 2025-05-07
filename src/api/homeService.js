4ca264941d8ebf07acc26f55c820792ae7d5a501
unknown <you@example.com>
2025-05-07 15:17:09 +0200

    cleaned up endpoint path and imports as per error messages in Vite + React

--- src/api/homeService.js
+++ src/api/homeService.js
@@ -2,7 +2,7 @@ import api from "./axios";
 
 export const getAllRentalPages = async () => {
     try {
-        const response = await api.get(`/rental/page/${id}`);
+        const response = await api.get(`/rental/page`);
     return response.data;
   } catch (error) {
     console.error("Error:", error);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,29 @@
 import "../styles/home.css";
-import IMG_1 from "../assets/IMG_1.png";
-import IMG_2 from "../assets/IMG_2.png";
-import IMG_3 from "../assets/IMG_3.png";
-import IMG_4 from "../assets/IMG_4.png";
-import IMG_5 from "../assets/IMG_5.png";
-import IMG_6 from "../assets/IMG_6.png";
+import { useEffect, useState } from "react";
+import { getAllRentalPages } from "../api/homeService";
+import { HomeListingCard } from "../components/HomeListing";
+
 // To check for logged in user
 import { useAuth } from "../hooks/useAuth";
-import HomeListing from "../components/HomeListing";
 
 const HomePage = () => {
   // const for logged in user
   const { isLoggedIn } = useAuth();
+  // const for list of all rentals
+  const [rentals, setRentals] = useState([]);
+
+  useEffect(() => {
+    const fetchRentals = async () => {
+      try {
+        const data = await getAllRentalPages();
+        setRentals(data);
+      } catch (error) {
+        console.error("Error fetching rentals:", error);
+      }
+    };
+
+    fetchRentals();
+  }, []); 
 
   return (
     <div className="home-container">
@@ -33,57 +45,12 @@ const HomePage = () => {
           </article>
           <br />
           </div>
-        </div>
       <div className="flex-grid">
-      <HomeListing
-  image={IMG_1}
-  altText="Rental 1"
-  imageClass="IMG_1"
-  location="Lake Bled, Slovenia"
-  title="Hush Cabin"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        <HomeListing
-  image={IMG_2}
-  altText="Rental 2"
-  imageClass="IMG_2"
-  location="Abisko, Sweden"
-  title="Midnight Sun Lodge"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        <HomeListing
-  image={IMG_3}
-  altText="Rental 3"
-  imageClass="IMG_3"
-  location="Bavaria, Germany"
-  title="Birch and Sky Treehouse"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        <HomeListing
-  image={IMG_4}
-  altText="Rental 4"
-  imageClass="IMG_4"
-  location="Kawartha Lakes, Canada"
-  title="Lakewind Lookout"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        <HomeListing
-  image={IMG_5}
-  altText="Rental 5"
-  imageClass="IMG_5"
-  location="Cotswolds, England"
-  title="Foxglove Hollow"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        <HomeListing
-  image={IMG_6}
-  altText="Rental 6"
-  imageClass="IMG_6"
-  location="Queenstown, New Zealand"
-  title="The Stargazer's Nest"
-  description="Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."/>
-        </div>
+      {rentals.map((rental) => (
+          <HomeListingCard key={rental.id} rental={rental} />
+        ))}
+      </div>
+      </div>
       </div>
   );
 };
