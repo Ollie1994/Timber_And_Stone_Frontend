c83cc8c3de99f0b78eb31f9c913fb9e80cf30b41
unknown <you@example.com>
2025-05-08 18:04:08 +0200

    switching branch

--- src/components/ReviewBox.jsx
+++ src/components/ReviewBox.jsx
@@ -3,22 +3,23 @@ import Button from "./Button";
 import { useState } from "react";
 
 const ReviewBox = () => {
-
   // Make the reviewbox hide some reviews
   const [expanded, setExpanded] = useState(false);
-    return (
-        <div className="reviews-container">
+  return (
+    <div className="reviews-container">
       <div className="review-container">
         <div className="reviews-top">
           <h2>Latest Reviews:</h2>
         </div>
-        <div className={`review-container ${expanded ? "expanded" : "collapsed"}`}>
-        <Review />
+        <div
+          className={`review-container ${expanded ? "expanded" : "collapsed"}`}
+        >
+          <Review />
         </div>
       </div>
       <Button onClick={() => setExpanded(!expanded)}>
-          <h4>{expanded ? "Show less" : "Show more"}</h4>
-        </Button>
+        <h4>{expanded ? "Show less" : "Show more"}</h4>
+      </Button>
     </div>
   );
 };
