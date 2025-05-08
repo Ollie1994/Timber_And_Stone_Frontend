12c9e9815708261e6eebe2ac60cea824a7a81cb3
unknown <you@example.com>
2025-05-08 12:30:29 +0200

    added expand section and show more button to ReviewBox

--- src/pages/RentalPage.jsx
+++ src/pages/RentalPage.jsx
@@ -2,9 +2,10 @@ import "../styles/rental.css";
 import Button from "../components/Button";
 import IconRow from "../components/IconRow";
 import RentalTitleSection from "../components/RentalTitleSection";
-import Review from "../components/Review";
 import ImageGrid from "../components/ImageGrid";
+import Review from "../components/Review";
 import Amenities from "../components/Amenities";
+import ReviewBox from "../components/ReviewBox";
 
 const RentalPage = () => {
   return (
@@ -71,7 +72,9 @@ const RentalPage = () => {
           <path d="M0 2H1280" stroke="#3C4634" strokeWidth="3" />
         </svg>
         <Amenities></Amenities>
-        <Review></Review>
+        <ReviewBox>
+          <Review></Review>
+        </ReviewBox>
       </div>
     </div>
   );
