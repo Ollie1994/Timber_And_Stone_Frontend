46c171251f69e0e230af1fb725e2e339e628ee46
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-05-17 13:22:16 +0200

    dev in and rethinkign design a bit

--- src/components/ProfileBookingTemplate.jsx
+++ src/components/ProfileBookingTemplate.jsx
@@ -12,11 +12,11 @@ const ProfileBookingTemplate = ({ booking }) => {
   return (
     <div className="profileBookingTemplate-profileBookingTemplateContainer">
      <div className="profileBookingTemplate-singleBookingContainer">
-      <div className="profileBookingTemplate-bookingNumberContainer"><h3>{booking.bookingNumber}</h3></div>
-      <div className="profileBookingTemplate-rentalTitleContainer"><h3>Rental: {booking.rental.title}</h3></div>
-      <div className="profileBookingTemplate-periodContainer"><h3>Period: {booking.period.startDate} - {booking.period.endDate}</h3></div>
-      <div className="profileBookingTemplate-isPaidContainer"><h3>isPaid: {booking.paid ? "True" : "False"}</h3></div>
-      <div className="profileBookingTemplate-bookingStatusContainer"><h3>Status: {booking.bookingStatus}</h3></div>
+      <div className="profileBookingTemplate-bookingNumberContainer"><h6>{booking.bookingNumber}</h6></div>
+      <div className="profileBookingTemplate-rentalTitleContainer"><h6>{booking.rental.title}</h6></div>
+      <div className="profileBookingTemplate-periodContainer"><h6>{booking.period.startDate} - {booking.period.endDate}</h6></div>
+      <div className="profileBookingTemplate-isPaidContainer"><h6>isPaid: {booking.paid ? "True" : "False"}</h6></div>
+      <div className="profileBookingTemplate-bookingStatusContainer"><h6>Status: {booking.bookingStatus}</h6></div>
 
      </div>
     </div>
